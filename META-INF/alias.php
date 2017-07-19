85edc690ea927dbb57c38c3ee558f847951e6101
xiaohengjin <xiaohengjin@youzan.com>
2017-07-19 21:08:51 +0800

    delete alias hawker

--- META-INF/alias.php
+++ META-INF/alias.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    "ZanPHP\\Contracts\\Hawk\\Hawker" => "Zan\\Framework\\Sdk\\Monitor\\Hawker",
     "ZanPHP\\Hawk\\Constant" => "Zan\\Framework\\Sdk\\Monitor\\Constant",
     "ZanPHP\\Hawk\\Hawk" => "Zan\\Framework\\Sdk\\Monitor\\Hawk"
 ];
