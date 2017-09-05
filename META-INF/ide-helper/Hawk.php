1c3acb1afb5b13e39c0744962270e08543d88ff2
huye <huye@youzan.com>
2017-09-05 14:39:10 +0800

    优化命名空间，支持IDE跳转

--- META-INF/ide-helper/Hawk.php
+++ META-INF/ide-helper/Hawk.php
@@ -4,64 +4,55 @@ namespace Zan\Framework\Sdk\Monitor\Hawk;
 
 class Hawk
 {
-    const SUCCESS_CODE = 200;
-    const URI = '/report';
+    private $Hawk;
 
-    const TOTAL_SUCCESS_TIME = 'totalSuccessTime';
-    const TOTAL_SUCCESS_COUNT = 'totalSuccessCount';
-    const MAX_SUCCESS_TIME = 'maxSuccessTime';
-    const TOTAL_FAILURE_TIME = 'totalFailureTime';
-    const TOTAL_FAILURE_COUNT = 'totalFailureCount';
-    const MAX_FAILURE_TIME = 'maxFailureTime';
-    const LIMIT_COUNT = 'limitCount';
-    const TOTAL_CONCURRENCY = 'totalConcurrency';
-    const CONCURRENCY_COUNT = 'concurrencyCount';
-
-    const CLIENT = 'client';
-    const SERVER = 'server';
+    public function __construct()
+    {
+        $this->Hawk = new \ZanPHP\Hawk\Hawk();
+    }
 
     public function run($server)
     {
-
+        $this->Hawk->run($server);
     }
 
     public function add($biz, array $metrics, array $tags = [])
     {
-
+        $this->Hawk->add($biz, $metrics, $tags);
     }
 
     public function report()
     {
-
+        $this->Hawk->report();
     }
 
     public function addServerServiceData($service, $method, $clientIp, $key, $val)
     {
-
+        $this->Hawk->addServerServiceData($service, $method, $clientIp, $key, $val);
     }
 
     public function addClientServiceData($service, $method, $serverIp, $key, $val)
     {
-
+        $this->Hawk->addClientServiceData($service, $method, $serverIp, $key, $val);
     }
 
     public function addTotalSuccessTime($side, $service, $method, $ip, $diffSec)
     {
-
+        $this->Hawk->addTotalSuccessTime($side, $service, $method, $ip, $diffSec);
     }
 
     public function addTotalFailureTime($side, $service, $method, $ip, $diffSec)
     {
-
+        $this->Hawk->addTotalFailureTime($side, $service, $method, $ip, $diffSec);
     }
 
     public function addTotalSuccessCount($side, $service, $method, $ip)
     {
-
+        $this->Hawk->addTotalSuccessCount($side, $service, $method, $ip);
     }
 
     public function addTotalFailureCount($side, $service, $method, $ip)
     {
-
+        $this->Hawk->addTotalFailureCount($side, $service, $method, $ip);
     }
 }
